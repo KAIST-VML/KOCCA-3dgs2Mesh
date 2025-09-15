69807d86b53386a088d596d5ad40e24715f3f845
SOLA <93892724+ffe4el@users.noreply.github.com>
2025-09-15 19:58:12 +0900

    Add SuGaR and gaussian-splatting as regular folders

---
+++ SuGaR/sugar_viewer/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: './',
+  build: {
+    target: 'esnext', //browsers can handle the latest ES features
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
+})
+
